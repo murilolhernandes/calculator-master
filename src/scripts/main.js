c46474f881104e62911e1b6a536099060aa679e9
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-06-06 13:24:23 -0600

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+import { loadCalculatorTemplate } from "./utils.mjs";
+
+loadCalculatorTemplate("../partials/calculatorDisplay.html", "#calculator");
+loadCalculatorTemplate("../partials/calculatorContainer.html", "#display-container");
+
+
---
+++ src/scripts/utils.mjs
@@ -0,0 +1,52 @@
+// wrapper for querySelector...returns matching element
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+// or a more concise version if you are into that sort of thing:
+// export const qs = (selector, parent = document) => parent.querySelector(selector);
+
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+// save data to local storage
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
+
+export async function loadCalculatorTemplate(path, element) {
+  const calculator = await loadTemplate(path);
+
+  const calculatorElement = document.querySelector(element);
+
+  renderWithTemplate(calculator, calculatorElement);
+}
+
+// export async function loadCalculatorContainer() {
+//   const calculatorContainer = await loadTemplate("../partials/calculatorContainer.html");
+
+//   const calculatorContainerElement = document.querySelector("#displayContainer");
+
+//   renderWithTemplate(calculatorContainer, calculatorContainerElement);
+// }
